aa239cbb5a991ffce00adb5aeb20ee374d2759d7
Alexander <alex.yashkin@gmail.com>
2015-04-22 15:01:51 +0300

    Prepare for Packagist

--- example.php
+++ example.php
@@ -4,9 +4,9 @@ $user = 'user@org.com'; // имя пользователя
 $pwd = 'paSSwo_rd'; // пароль
 $is_test = true; // тестовый api
 
-require 'HootkiGrosh.php';
+require 'src/HootkiGrosh.php';
 
-$hg = new HootkiGrosh($is_test);
+$hg = new \Alexantr\HootkiGrosh\HootkiGrosh($is_test);
 
 /*
  * Авторизация
@@ -16,9 +16,9 @@ $res = $hg->apiLogIn($user, $pwd);
 
 // Ошибка авторизации
 if (!$res) {
-	echo $hg->getError();
-	$hg->apiLogOut(); // Завершаем сеанс
-	exit;
+    echo $hg->getError();
+    $hg->apiLogOut(); // Завершаем сеанс
+    exit;
 }
 
 /*
@@ -26,34 +26,34 @@ if (!$res) {
  */
 
 $data = array(
-	'eripId'      => '40000001',
-	'invId'       => 'C-1234',
-	'fullName'    => 'Пупкин Василий Иванович',
-	'mobilePhone' => '+333 33 3332221',
-	'email'       => 'pupkin@org.com',
-	'fullAddress' => 'г.Минск, пр.Победителей, д.1, кв.1',
-	'amt'         => 120000,
-	'products'    => array(
-		array(
-			'invItemId' => 'Артикул 123',
-			'desc'      => 'Услуга, за которую производят оплату',
-			'count'     => 1,
-			'amt'       => 119000,
-		),
-		array(
-			'invItemId' => '-нет-',
-			'desc'      => 'Доставка',
-			'count'     => 1,
-			'amt'       => 1000,
-		),
-	),
+    'eripId' => '40000001',
+    'invId' => 'C-1234',
+    'fullName' => 'Пупкин Василий Иванович',
+    'mobilePhone' => '+333 33 3332221',
+    'email' => 'pupkin@org.com',
+    'fullAddress' => 'г.Минск, пр.Победителей, д.1, кв.1',
+    'amt' => 120000,
+    'products' => array(
+        array(
+            'invItemId' => 'Артикул 123',
+            'desc' => 'Услуга, за которую производят оплату',
+            'count' => 1,
+            'amt' => 119000,
+        ),
+        array(
+            'invItemId' => '-нет-',
+            'desc' => 'Доставка',
+            'count' => 1,
+            'amt' => 1000,
+        ),
+    ),
 );
 
 $billID = $hg->apiBillNew($data);
 if (!$billID) {
-	echo $hg->getError();
-	$hg->apiLogOut(); // Завершаем сеанс
-	exit;
+    echo $hg->getError();
+    $hg->apiLogOut(); // Завершаем сеанс
+    exit;
 }
 echo 'bill ID: ' . $billID . '<br>';
 
@@ -63,9 +63,9 @@ echo 'bill ID: ' . $billID . '<br>';
 
 $status = $hg->apiBillStatus($billID);
 if (!$status) {
-	echo $hg->getError();
-	$hg->apiLogOut(); // Завершаем сеанс
-	exit;
+    echo $hg->getError();
+    $hg->apiLogOut(); // Завершаем сеанс
+    exit;
 }
 echo 'Статус: ' . $status . ' (' . $hg->getPurchItemStatus($status) . ')<br>';
 
@@ -75,9 +75,9 @@ echo 'Статус: ' . $status . ' (' . $hg->getPurchItemStatus($status) . ')<b
 
 $info = $hg->apiBillInfo($billID);
 if (!$info) {
-	echo $hg->getError();
-	$hg->apiLogOut(); // Завершаем сеанс
-	exit;
+    echo $hg->getError();
+    $hg->apiLogOut(); // Завершаем сеанс
+    exit;
 }
 echo '<pre>' . print_r($info, true) . '</pre>';
 
@@ -87,9 +87,9 @@ echo '<pre>' . print_r($info, true) . '</pre>';
 
 $res = $hg->apiBillDelete($billID);
 if (!$res) {
-	echo $hg->getError();
-	$hg->apiLogOut(); // Завершаем сеанс
-	exit;
+    echo $hg->getError();
+    $hg->apiLogOut(); // Завершаем сеанс
+    exit;
 }
 
 /*
