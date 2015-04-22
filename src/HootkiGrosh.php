57094201b42efbd995d75f42e43754f99f190571
Alexander <alex.yashkin@gmail.com>
2015-04-22 15:43:55 +0300

    Remove underscore

--- src/HootkiGrosh.php
+++ src/HootkiGrosh.php
@@ -9,21 +9,21 @@ namespace Alexantr\HootkiGrosh;
  */
 class HootkiGrosh
 {
-    private static $_cookies_file; // имя файла с cookies
+    private static $cookies_file; // имя файла с cookies
 
-    private $_base_url; // url api
+    private $base_url; // url api
 
-    private $_ch; // curl object
-    private $_error; // ошибка запроса (если есть)
-    private $_response; // тело ответа
-    private $_status; // код статуса
+    private $ch; // curl object
+    private $error; // ошибка запроса (если есть)
+    private $response; // тело ответа
+    private $status; // код статуса
 
     // api url
-    private $_api_url = 'https://www.hutkigrosh.by/API/v1/'; // рабочий
-    private $_test_api_url = 'https://trial.hgrosh.by/API/v1/'; // тестовый
+    private $api_url = 'https://www.hutkigrosh.by/API/v1/'; // рабочий
+    private $test_api_url = 'https://trial.hgrosh.by/API/v1/'; // тестовый
 
     // Список ошибок
-    private $_status_error = array(
+    private $status_error = array(
         '3221291009' => 'Общая ошибка сервиса',
         '3221291521' => 'Нет информации о счете',
         '3221291522' => 'Нет возможности удалить счет',
@@ -45,7 +45,7 @@ class HootkiGrosh
     );
 
     // Список статусов счета
-    private $_purch_item_status = array(
+    private $purch_item_status = array(
         'NotSet' => 'Не установлено',
         'PaymentPending' => 'Ожидание оплаты',
         'Outstending' => 'Просроченный',
@@ -55,7 +55,7 @@ class HootkiGrosh
     );
 
     // Доступные валюты
-    private $_currencies = array('BYR', 'USD', 'EUR', 'RUB');
+    private $currencies = array('BYR', 'USD', 'EUR', 'RUB');
 
     /**
      * @param bool $is_test Использовать ли тестовый api
@@ -63,13 +63,13 @@ class HootkiGrosh
     public function __construct($is_test = false)
     {
         if ($is_test) {
-            $this->_base_url = $this->_test_api_url;
+            $this->base_url = $this->test_api_url;
         } else {
-            $this->_base_url = $this->_api_url;
+            $this->base_url = $this->api_url;
         }
 
-        if (!isset(self::$_cookies_file)) {
-            self::$_cookies_file = 'cookies-' . time() . '.txt';
+        if (!isset(self::$cookies_file)) {
+            self::$cookies_file = 'cookies-' . time() . '.txt';
         }
     }
 
@@ -92,11 +92,11 @@ class HootkiGrosh
         $xml = $Credentials->asXML();
 
         // запрос
-        $res = $this->_requestPost('Security/LogIn', $xml);
+        $res = $this->requestPost('Security/LogIn', $xml);
 
         // проверим, верны ли логин/пароль
-        if ($res && !preg_match('/true/', $this->_response)) {
-            $this->_error = 'Ошибка авторизации';
+        if ($res && !preg_match('/true/', $this->response)) {
+            $this->error = 'Ошибка авторизации';
             return false;
         }
 
@@ -109,10 +109,10 @@ class HootkiGrosh
      */
     public function apiLogOut()
     {
-        $res = $this->_requestPost('Security/LogOut');
+        $res = $this->requestPost('Security/LogOut');
         // удалим файл с cookies
-        if (is_file(self::$_cookies_file)) {
-            @unlink(self::$_cookies_file);
+        if (is_file(self::$cookies_file)) {
+            @unlink(self::$cookies_file);
         }
         return $res;
     }
@@ -128,8 +128,8 @@ class HootkiGrosh
     {
         // выберем валюту
         $curr = isset($data['curr']) ? trim($data['curr']) : 'BYR';
-        if (!in_array($curr, $this->_currencies)) {
-            $curr = $this->_currencies[0];
+        if (!in_array($curr, $this->currencies)) {
+            $curr = $this->currencies[0];
         }
 
         // формируем xml
@@ -173,24 +173,24 @@ class HootkiGrosh
         $xml = $Bill->asXML();
 
         // запрос
-        $res = $this->_requestPost('Invoicing/Bill', $xml);
+        $res = $this->requestPost('Invoicing/Bill', $xml);
 
         if ($res) {
-            $array = $this->_responseToArray();
+            $array = $this->responseToArray();
 
             if (is_array($array) && isset($array['status']) && isset($array['billID'])) {
-                $this->_status = (int)$array['status'];
+                $this->status = (int)$array['status'];
                 $billID = trim("{$array['billID']}");
 
                 // есть ошибка
-                if ($this->_status > 0) {
-                    $this->_error = $this->_getStatusError($this->_status);
+                if ($this->status > 0) {
+                    $this->error = $this->getStatusError($this->status);
                     return false;
                 }
 
                 return $billID;
             } else {
-                $this->_error = 'Неверный ответ сервера';
+                $this->error = 'Неверный ответ сервера';
             }
         }
 
@@ -207,24 +207,24 @@ class HootkiGrosh
     public function apiBillInfo($bill_id)
     {
         // запрос
-        $res = $this->_requestGet('Invoicing/Bill(' . $bill_id . ')');
+        $res = $this->requestGet('Invoicing/Bill(' . $bill_id . ')');
 
         if ($res) {
-            $array = $this->_responseToArray();
+            $array = $this->responseToArray();
 
             if (is_array($array) && isset($array['status']) && isset($array['bill'])) {
-                $this->_status = (int)$array['status'];
+                $this->status = (int)$array['status'];
                 $bill = (array)$array['bill'];
 
                 // есть ошибка
-                if ($this->_status > 0) {
-                    $this->_error = $this->_getStatusError($this->_status);
+                if ($this->status > 0) {
+                    $this->error = $this->getStatusError($this->status);
                     return false;
                 }
 
                 return $bill;
             } else {
-                $this->_error = 'Неверный ответ сервера';
+                $this->error = 'Неверный ответ сервера';
             }
         }
 
@@ -240,24 +240,24 @@ class HootkiGrosh
      */
     public function apiBillDelete($bill_id)
     {
-        $res = $this->_requestDelete('Invoicing/Bill(' . $bill_id . ')');
+        $res = $this->requestDelete('Invoicing/Bill(' . $bill_id . ')');
 
         if ($res) {
-            $array = $this->_responseToArray();
+            $array = $this->responseToArray();
 
             if (is_array($array) && isset($array['status']) && isset($array['purchItemStatus'])) {
-                $this->_status = (int)$array['status'];
+                $this->status = (int)$array['status'];
                 $purchItemStatus = trim($array['purchItemStatus']); // статус счета
 
                 // есть ошибка
-                if ($this->_status > 0) {
-                    $this->_error = $this->_getStatusError($this->_status);
+                if ($this->status > 0) {
+                    $this->error = $this->getStatusError($this->status);
                     return false;
                 }
 
                 return $purchItemStatus;
             } else {
-                $this->_error = 'Неверный ответ сервера';
+                $this->error = 'Неверный ответ сервера';
             }
         }
 
@@ -273,24 +273,24 @@ class HootkiGrosh
      */
     public function apiBillStatus($bill_id)
     {
-        $res = $this->_requestGet('Invoicing/BillStatus(' . $bill_id . ')');
+        $res = $this->requestGet('Invoicing/BillStatus(' . $bill_id . ')');
 
         if ($res) {
-            $array = $this->_responseToArray();
+            $array = $this->responseToArray();
 
             if (is_array($array) && isset($array['status']) && isset($array['purchItemStatus'])) {
-                $this->_status = (int)$array['status'];
+                $this->status = (int)$array['status'];
                 $purchItemStatus = trim($array['purchItemStatus']); // статус счета
 
                 // есть ошибка
-                if ($this->_status > 0) {
-                    $this->_error = $this->_getStatusError($this->_status);
+                if ($this->status > 0) {
+                    $this->error = $this->getStatusError($this->status);
                     return false;
                 }
 
                 return $purchItemStatus;
             } else {
-                $this->_error = 'Неверный ответ сервера';
+                $this->error = 'Неверный ответ сервера';
             }
         }
 
@@ -304,7 +304,7 @@ class HootkiGrosh
      */
     public function getError()
     {
-        return $this->_error;
+        return $this->error;
     }
 
     /**
@@ -314,7 +314,7 @@ class HootkiGrosh
      */
     public function getResponse()
     {
-        return $this->_response;
+        return $this->response;
     }
 
     /**
@@ -324,7 +324,7 @@ class HootkiGrosh
      */
     public function getStatus()
     {
-        return $this->_status;
+        return $this->status;
     }
 
     /**
@@ -336,7 +336,7 @@ class HootkiGrosh
      */
     public function getPurchItemStatus($status)
     {
-        return (isset($this->_purch_item_status[$status])) ? $this->_purch_item_status[$status] : 'Статус не определен';
+        return (isset($this->purch_item_status[$status])) ? $this->purch_item_status[$status] : 'Статус не определен';
     }
 
     /**
@@ -347,9 +347,9 @@ class HootkiGrosh
      *
      * @return bool
      */
-    private function _requestGet($path, $data = '')
+    private function requestGet($path, $data = '')
     {
-        return $this->_connect($path, $data, 'GET');
+        return $this->connect($path, $data, 'GET');
     }
 
     /**
@@ -360,9 +360,9 @@ class HootkiGrosh
      *
      * @return bool
      */
-    private function _requestPost($path, $data = '')
+    private function requestPost($path, $data = '')
     {
-        return $this->_connect($path, $data, 'POST');
+        return $this->connect($path, $data, 'POST');
     }
 
     /**
@@ -373,9 +373,9 @@ class HootkiGrosh
      *
      * @return bool
      */
-    private function _requestDelete($path, $data = '')
+    private function requestDelete($path, $data = '')
     {
-        return $this->_connect($path, $data, 'DELETE');
+        return $this->connect($path, $data, 'DELETE');
     }
 
     /**
@@ -387,41 +387,41 @@ class HootkiGrosh
      *
      * @return bool
      */
-    private function _connect($path, $data = '', $request = 'GET')
+    private function connect($path, $data = '', $request = 'GET')
     {
         $headers = array('Content-Type: application/xml', 'Content-Length: ' . strlen($data));
 
-        $this->_ch = curl_init();
+        $this->ch = curl_init();
 
-        curl_setopt($this->_ch, CURLOPT_URL, $this->_base_url . $path);
-        curl_setopt($this->_ch, CURLOPT_HEADER, false); // включение заголовков в выводе
-        curl_setopt($this->_ch, CURLOPT_VERBOSE, true); // вывод доп. информации в STDERR
-        curl_setopt($this->_ch, CURLOPT_SSL_VERIFYPEER, false); // не проверять сертификат узла сети
-        curl_setopt($this->_ch, CURLOPT_SSL_VERIFYHOST, false); // проверка существования общего имени в сертификате SSL
-        curl_setopt($this->_ch, CURLOPT_RETURNTRANSFER, true); // возврат результата вместо вывода на экран
-        curl_setopt($this->_ch, CURLOPT_HTTPHEADER, $headers); // Массив устанавливаемых HTTP-заголовков
+        curl_setopt($this->ch, CURLOPT_URL, $this->base_url . $path);
+        curl_setopt($this->ch, CURLOPT_HEADER, false); // включение заголовков в выводе
+        curl_setopt($this->ch, CURLOPT_VERBOSE, true); // вывод доп. информации в STDERR
+        curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, false); // не проверять сертификат узла сети
+        curl_setopt($this->ch, CURLOPT_SSL_VERIFYHOST, false); // проверка существования общего имени в сертификате SSL
+        curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true); // возврат результата вместо вывода на экран
+        curl_setopt($this->ch, CURLOPT_HTTPHEADER, $headers); // Массив устанавливаемых HTTP-заголовков
         if ($request == 'POST') {
-            curl_setopt($this->_ch, CURLOPT_POST, true);
-            curl_setopt($this->_ch, CURLOPT_POSTFIELDS, $data);
+            curl_setopt($this->ch, CURLOPT_POST, true);
+            curl_setopt($this->ch, CURLOPT_POSTFIELDS, $data);
         }
         if ($request == 'DELETE') {
-            curl_setopt($this->_ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+            curl_setopt($this->ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
         }
 
         // если файла еще нет, то создадим его при залогинивании и будем затем использовать при дальнейших запросах
-        if (!is_file(self::$_cookies_file)) {
-            curl_setopt($this->_ch, CURLOPT_COOKIEJAR, self::$_cookies_file);
+        if (!is_file(self::$cookies_file)) {
+            curl_setopt($this->ch, CURLOPT_COOKIEJAR, self::$cookies_file);
         }
-        curl_setopt($this->_ch, CURLOPT_COOKIEFILE, self::$_cookies_file);
+        curl_setopt($this->ch, CURLOPT_COOKIEFILE, self::$cookies_file);
 
-        $this->_response = curl_exec($this->_ch);
+        $this->response = curl_exec($this->ch);
 
-        if (curl_errno($this->_ch)) {
-            $this->_error = curl_error($this->_ch);
-            curl_close($this->_ch);
+        if (curl_errno($this->ch)) {
+            $this->error = curl_error($this->ch);
+            curl_close($this->ch);
             return false;
         } else {
-            curl_close($this->_ch);
+            curl_close($this->ch);
             return true;
         }
     }
@@ -431,9 +431,9 @@ class HootkiGrosh
      *
      * @return mixed
      */
-    private function _responseToArray()
+    private function responseToArray()
     {
-        $response = trim($this->_response);
+        $response = trim($this->response);
         $array = array();
         // проверим, что это xml
         if (preg_match('/^<(.*)>$/', $response)) {
@@ -450,8 +450,8 @@ class HootkiGrosh
      *
      * @return string
      */
-    private function _getStatusError($status)
+    private function getStatusError($status)
     {
-        return (isset($this->_status_error[$status])) ? $this->_status_error[$status] : 'Неизвестная ошибка';
+        return (isset($this->status_error[$status])) ? $this->status_error[$status] : 'Неизвестная ошибка';
     }
 }
